c78d437931a1c72a0feffd45fb4d027c51d8ee36
arimorcos <arimorcos@gmail.com>
2015-12-18 15:38:02 -0500

    Added image display helpers

--- notebooks/tiff_helpers.py
+++ notebooks/tiff_helpers.py
@@ -1,6 +1,7 @@
 import numpy as np
 from tifffile import TiffFile
 import warnings
+import matplotlib.pyplot as plt
 
 
 def read_tiff(path, pages=None):
@@ -61,3 +62,38 @@ def get_num_tiff_pages(path):
     with TiffFile(path) as f:
         return len(f.pages)
 
+
+def imshowpair(img_1, img_2, ax=None):
+    """
+    Shows images as an overlapping pair with image 1 in red and image 2 in green
+    :param img_1: first image to display. Will be shown in red.
+    :param img_2: second image to display. Will be shown in green.
+    :param ax: axis to plot on. If none, current.
+    :return: None
+    """
+
+    if ax is None:
+        ax = plt.gca()
+
+    # Create zero image
+    zero_pad = np.zeros(shape=img_1.shape)
+
+    # Normalize images
+    img_1 = norm_img(img_1)
+    img_2 = norm_img(img_2)
+
+    # Concatenate image
+    show_img = np.stack((img_1, img_2, zero_pad), axis=2)
+
+    # Plot
+    ax.imshow(show_img)
+
+
+def norm_img(img):
+    """
+    Normalizes an image to between 0 and 1
+    :param img: image to normalize
+    :return: nomralized image
+    """
+
+    return (img - img.min())/(img.max() - img.min())
