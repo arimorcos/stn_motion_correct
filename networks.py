bd7781c6c4fe96f93e36e02ec35a864e13df1728
arimorcos <arimorcos@gmail.com>
2016-01-15 09:58:41 -0500

    Moved basics of thin plate spline transformer to a separate function

--- networks.py
+++ networks.py
@@ -9,7 +9,6 @@ import sys
 import warnings
 import logging
 from batch_norm import batch_norm
-from tps import from_control_points
 from abc import ABCMeta, abstractmethod
 
 
@@ -574,42 +573,6 @@ class stn_tps(generic_stn):
 
         # create function
         self.get_cost = theano.function([self.input_batch, self.ref_imgs], self.cost)
-
-    def apply_tps_transform(self, img, control_points):
-        """
-        Applies the thin-plate spline transform to the image provided and outputs the transformed image
-        :param img: The image to be transformed
-        :return: transformed image with the same dimensions as the input image
-        """
-
-        # get the image dimensions
-        height, width = img.shape
-
-        # Generate the start control points
-        grid_size = np.sqrt(self.num_control_points)
-        x_control_t, y_control_t = np.meshgrid(np.linspace(-1, 1, grid_size),
-                               np.linspace(-1, 1, grid_size))
-
-        # Create control matrix comprising a list of 4-element lists, with the following elements:
-        # [source_x, source_y, dest_x, dest_y]
-        control_points = np.reshape(control_points, (-1, 2))
-        full_array = np.vstack((x_control_t.flatten(), y_control_t.flatten(), control_points)).tolist()
-
-        # Create the transformer object
-        tps_transformer = from_control_points(full_array)
-
-        # Generate the input grid
-        x_t, y_t = np.meshgrid(np.linspace(-1, 1, height),
-                               np.linspace(-1, 1, width))
-        x_t = x_t.flatten()
-        y_t = y_t.flatten()
-
-        # transform each point pair
-        x_s = np.empty(x_t.shape)
-        y_s = np.empty(y_t.shape)
-        for ind, (x_coord, y_coord) in enumerate(zip(x_t, y_t)):
-            x_s[ind], y_s[ind] = tps_transformer.transform(x_coord, y_coord)
-
 
     def initialize_process(self):
         """
